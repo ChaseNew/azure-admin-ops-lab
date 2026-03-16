86b0d397596b0033c515985a89bb147d92360204
ChaseNew <chase@newfolio.com>
2026-03-16 14:58:25 -0400

    Update deploy.ps1

--- iac/scripts/deploy.ps1
+++ iac/scripts/deploy.ps1
@@ -1,1 +1,20 @@
+# deploy.ps1
+# Deploys the Azure infrastructure defined in the Bicep template.
 
+# Login to Azure
+Connect-AzAccount
+
+# Variables
+$resourceGroup = "portfolio-rg"
+$location = "canadacentral"
+
+# Create resource group
+New-AzResourceGroup `
+  -Name $resourceGroup `
+  -Location $location
+
+# Deploy infrastructure using Bicep
+New-AzResourceGroupDeployment `
+  -ResourceGroupName $resourceGroup `
+  -TemplateFile "../iac/bicep/main.bicep" `
+  -TemplateParameterFile "../iac/bicep/parameters.json"
